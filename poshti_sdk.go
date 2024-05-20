fac4e1078c683b1f79c6c4dc88af94eba9cc43e3
ERFAN <sadeqierfan00@gmail.com>
2024-05-20 18:48:50 +0330

    Define initial structures for Poshti WebSocket SDK: Message, Subscription, and Client

---
+++ poshti_sdk.go
@@ -0,0 +1,32 @@
+package poshti_sdk
+
+import (
+	"encoding/json"
+    "fmt"
+    "log"
+	"sync"
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	JoinRef string			`json:"join_ref"`
+	MsgRef  string          `json:"msg_ref"`
+	Topic   string          `json:"topic"`
+	Event   string          `json:"event"`
+	Payload json.RawMessage `json:"payload"`
+}
+
+type Subscription struct {
+	Topic string
+	Callback func(msg Message)
+}
+
+type Client struct {
+	conn *websocket.Conn
+	url string
+	mu sync.Mutex 
+	messageChan chan Message
+	subscriptions map[string]Subscription
+}
+
+
